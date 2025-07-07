d25d2442bae9aff558766a29e6130e435d2db42c
Natalia Muñiz <nati12n@gmail.com>
2025-07-08 01:17:56 +0200

    Changed global variables from static to extern.

--- Zork/Globals.cpp
+++ Zork/Globals.cpp
@@ -1,10 +1,35 @@
 #include "Globals.h"
 
+bool global_end;
+bool global_dead;
+bool global_isVerbose;
+
+bool global_mandrakeGiven;
+bool global_spellLearned;
+bool global_coinsGiven;
+bool global_climbedWell;
+bool global_syrenDefeated;
+bool global_visitedMagicalForest;
+bool global_gotUnicorn;
+
 string Globals_ToUpper(string _input)
 {
 	for (char& c : _input)
 	{
 		c = (char)toupper(c);
+	}
+
+	return _input;
+}
+
+string Globals_ToNormalized(string _input)
+{
+	bool _isFirst = true;
+
+	for (char& c : _input)
+	{
+		c = _isFirst ? (char)toupper(c) : (char)tolower(c);
+		_isFirst = false;
 	}
 
 	return _input;
--- Zork/Globals.h
+++ Zork/Globals.h
@@ -2,18 +2,20 @@
 #include <string>
 using namespace std;
 
-static bool global_end;
-static bool global_dead;
-static bool global_isVerbose;
+extern bool global_end;
+extern bool global_dead;
+extern bool global_isVerbose;
 
-static bool global_mandrakeGiven;
-static bool global_spellLearned;
-static bool global_coinsGiven;
-static bool global_climbedWell;
-static bool global_syrenDefeated;
-static bool global_visitedMagicalForest;
-static bool global_gotUnicorn;
+extern bool global_mandrakeGiven;
+extern bool global_spellLearned;
+extern bool global_coinsGiven;
+extern bool global_climbedWell;
+extern bool global_syrenDefeated;
+extern bool global_visitedMagicalForest;
+extern bool global_gotUnicorn;
 
 string Globals_ToUpper(string _string);
+string Globals_ToNormalized(string _string);
+
 
 
