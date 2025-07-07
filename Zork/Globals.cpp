723703862435992d261f371c0b0d2bbe8997b774
Natalia Muñiz <nati12n@gmail.com>
2025-07-07 02:28:48 +0200

    Changes to globals
    -Added victory conditions
    -Added function to convert string to uppercase.

--- Zork/Globals.cpp
+++ Zork/Globals.cpp
@@ -1,1 +1,11 @@
-//#include "Globals.h"
+#include "Globals.h"
+
+string Globals_ToUpper(string _input)
+{
+	for (char& c : _input)
+	{
+		c = (char)toupper(c);
+	}
+
+	return _input;
+}
--- Zork/Globals.h
+++ Zork/Globals.h
@@ -1,6 +1,19 @@
 #pragma once
+#include <string>
+using namespace std;
 
-bool global_end;
-bool isVerbose;
+static bool global_end;
+static bool global_dead;
+static bool global_isVerbose;
+
+static bool global_mandrakeGiven;
+static bool global_spellLearned;
+static bool global_coinsGiven;
+static bool global_climbedWell;
+static bool global_syrenDefeated;
+static bool global_visitedMagicalForest;
+static bool global_gotUnicorn;
+
+string Globals_ToUpper(string _string);
 
 
